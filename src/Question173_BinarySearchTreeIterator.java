765e630c40d1d63f322fd389f90978a8f2b96b90
Zhang Lei <942471854@qq.com>
2021-03-28 01:48:46 +0800

    Question173_Binary Search Tree Iterator

---
+++ src/Question173_BinarySearchTreeIterator.java
@@ -0,0 +1,81 @@
+import java.util.Stack;
+
+/**
+ * 173. 二叉搜索树迭代器
+ * 实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+ * 调用 next() 将返回二叉搜索树中的下一个最小的数。
+ * <p>
+ * 示例：
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // 返回 3
+ * iterator.next();    // 返回 7
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 9
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 15
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 20
+ * iterator.hasNext(); // 返回 false
+ * <p>
+ * 提示：
+ * next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。
+ */
+
+public class Question173_BinarySearchTreeIterator {
+	public static void main(String[] args) {
+
+	}
+}
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+	Stack<TreeNode> stack = new Stack<>();
+
+	public BSTIterator(TreeNode root) {
+		TreeNode p = root;
+		stack.push(p);
+		p = p.left;
+		while(p != null) {
+			stack.push(p);
+			p = p.left;
+		}
+	}
+
+	public int next() {
+		TreeNode p = stack.pop();
+		int value = p.val;
+		p = p.right;
+		while(p != null) {
+			stack.push(p);
+			p = p.left;
+		}
+
+		return value;
+	}
+
+	public boolean hasNext() {
+		return !stack.empty();
+	}
+}
+
+/*
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
