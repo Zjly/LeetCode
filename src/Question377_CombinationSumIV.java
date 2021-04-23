b4e47b894d34b0ae2f9aea0e5de01468f0e52123
Zhang Lei <942471854@qq.com>
2021-04-24 00:44:26 +0800

    Question377_Combination Sum IV

---
+++ src/Question377_CombinationSumIV.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+/**
+ * 377. 组合总和 Ⅳ
+ * 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
+ * 题目数据保证答案符合 32 位整数范围。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [1,2,3], target = 4
+ * 输出：7
+ * 解释：
+ * 所有可能的组合为：
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ * 请注意，顺序不同的序列被视作不同的组合。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [9], target = 3
+ * 输出：0
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 1000
+ * nums 中的所有元素 互不相同
+ * 1 <= target <= 1000
+ * <p>
+ * 进阶：如果给定的数组中含有负数会发生什么？问题会产生何种变化？如果允许负数出现，需要向题目中添加哪些限制条件？
+ */
+
+public class Question377_CombinationSumIV {
+	public static void main(String[] args) {
+		int[] nums = {4, 2, 1};
+		int target = 32;
+		Solution377 solution377 = new Solution377();
+		System.out.println(solution377.combinationSum4(nums, target));
+	}
+}
+
+class Solution377 {
+	public int combinationSum4(int[] nums, int target) {
+		int[] count = new int[target + 1];
+		count[target] = 1;
+
+		for(int i = target; i >= 0; i--) {
+			for(int num : nums) {
+				if(i - num >= 0) {
+				    count[i - num] += count[i];
+				}
+			}
+		}
+
+		return count[0];
+	}
+}
