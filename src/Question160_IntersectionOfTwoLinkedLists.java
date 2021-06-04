52a408104a50a00437655bb9793351d56848ff5c
Zhang Lei <942471854@qq.com>
2021-06-04 10:38:26 +0800

    Question160_Intersection Of Two Linked Lists

---
+++ src/Question160_IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,62 @@
+import java.util.HashSet;
+
+/**
+ * 160. 相交链表
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ * 图示两个链表在节点 c1 开始相交：
+ * 题目数据 保证 整个链式结构中不存在环。
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * <p>
+ * 示例 1：
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ * <p>
+ * 示例 2：
+ * 输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+ * 输出：Intersected at '2'
+ * 解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。
+ * 在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ */
+
+public class Question160_IntersectionOfTwoLinkedLists {
+	public static void main(String[] args) {
+
+	}
+}
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+class Solution160 {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		HashSet<ListNode> hashSet = new HashSet<>();
+		ListNode p = headA;
+		while(p != null) {
+			hashSet.add(p);
+			p = p.next;
+		}
+
+		p = headB;
+		while(p != null) {
+			if(hashSet.contains(p)) {
+				return p;
+			}
+			p = p.next;
+		}
+
+		return null;
+	}
+}
