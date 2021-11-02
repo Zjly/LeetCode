a6e92661a640a03cdec52cb72ab11b51e6b3279d
Zhang Lei <942471854@qq.com>
2021-11-02 10:53:14 +0800

    Question237_Delete Node In A LinkedList

---
+++ src/Question237_DeleteNodeInALinkedList.java
@@ -0,0 +1,70 @@
+/**
+ * 237. 删除链表中的节点
+ * 请编写一个函数，用于 删除单链表中某个特定节点 。在设计函数时需要注意，你无法访问链表的头节点 head ，只能直接访问 要被删除的节点 。
+ * <p>
+ * 题目数据保证需要删除的节点 不是末尾节点 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [4,5,1,9], node = 5
+ * 输出：[4,1,9]
+ * 解释：指定链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [4,5,1,9], node = 1
+ * 输出：[4,5,9]
+ * 解释：指定链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9
+ * 示例 3：
+ * <p>
+ * 输入：head = [1,2,3,4], node = 3
+ * 输出：[1,2,4]
+ * 示例 4：
+ * <p>
+ * 输入：head = [0,1], node = 0
+ * 输出：[1]
+ * 示例 5：
+ * <p>
+ * 输入：head = [-3,5,-99], node = -3
+ * 输出：[5,-99]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 链表中节点的数目范围是 [2, 1000]
+ * -1000 <= Node.val <= 1000
+ * 链表中每个节点的值都是唯一的
+ * 需要删除的节点 node 是 链表中的一个有效节点 ，且 不是末尾节点
+ */
+
+public class Question237_DeleteNodeInALinkedList {
+	public static void main(String[] args) {
+
+	}
+}
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution237 {
+	public void deleteNode(ListNode node) {
+		ListNode p = node;
+		ListNode q = node.next;
+		while(q.next != null) {
+			p.val = q.val;
+			p = p.next;
+			q = q.next;
+		}
+
+		p.val = q.val;
+		p.next = null;
+	}
+}
