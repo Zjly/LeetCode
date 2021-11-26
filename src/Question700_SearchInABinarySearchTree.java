7ad61e644c1b2ba6385c145fad439cfcab2b347d
Zhang Lei <942471854@qq.com>
2021-11-26 10:05:02 +0800

    Question700_Search In A Binary Search Tree

---
+++ src/Question700_SearchInABinarySearchTree.java
@@ -0,0 +1,61 @@
+/**
+ * 700. 二叉搜索树中的搜索
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ * <p>
+ * 例如，
+ * <p>
+ * 给定二叉搜索树:
+ * <p>
+ * 4
+ * / \
+ * 2   7
+ * / \
+ * 1   3
+ * <p>
+ * 和值: 2
+ * 你应该返回如下子树:
+ * <p>
+ * 2
+ * / \
+ * 1   3
+ * 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+ */
+
+public class Question700_SearchInABinarySearchTree {
+	public static void main(String[] args) {
+
+	}
+}
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution700 {
+	public TreeNode searchBST(TreeNode root, int val) {
+		TreeNode p = root;
+
+		while(p != null) {
+		    if(p.val > val) {
+		        p = p.left;
+		    } else if(p.val < val) {
+		        p = p.right;
+		    } else {
+		        return p;
+		    }
+		}
+
+		return null;
+	}
+}
