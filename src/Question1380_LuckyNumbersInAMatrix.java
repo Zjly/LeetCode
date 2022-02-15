b7c83a1eceaa2210c21f0d4988e410f96c323b50
Zhang Lei <942471854@qq.com>
2022-02-15 14:05:48 +0800

    Question1380_Lucky Numbers In A Matrix

--- src/Question1380_LuckyNumbersInAMatrix.java
+++ src/Question1380_LuckyNumbersInAMatrix.java
@@ -33,42 +33,63 @@
  * 矩阵中的所有元素都是不同的
  */
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Question1380_LuckyNumbersInAMatrix {
 	public static void main(String[] args) {
-
+		Solution1380 solution1380 = new Solution1380();
+		int[][] matrix = {{3, 7, 8}, {9, 11, 13}, {15, 16, 17}};
+		System.out.println(solution1380.luckyNumbers(matrix));
 	}
 }
 
 class Solution1380 {
 	public List<Integer> luckyNumbers(int[][] matrix) {
-		HashMap<Integer, Integer> rowValueIndexHashMap = new HashMap<>();
-		HashMap<Integer, Integer> columnValueIndexHashMap = new HashMap<>();
+		ArrayList<int[]> rowValueIndexArrayList = new ArrayList<>();
+		ArrayList<int[]> columnValueIndexArrayList = new ArrayList<>();
 
 		for(int i = 0; i < matrix.length; i++) {
-			int min = Integer.MAX_VALUE;
+			int minValue = Integer.MAX_VALUE;
 			int minIndex = 0;
 			for(int j = 0; j < matrix[0].length; j++) {
-				if(matrix[i][j] < min) {
-					min = matrix[i][j];
+				if(matrix[i][j] < minValue) {
+					minValue = matrix[i][j];
 					minIndex = j;
 				}
 			}
 
-			rowValueIndexHashMap.put(min, minIndex);
+			rowValueIndexArrayList.add(new int[]{minValue, minIndex});
 		}
 
 		for(int i = 0; i < matrix[0].length; i++) {
 			int maxValue = Integer.MIN_VALUE;
 			int maxIndex = 0;
-			for(int j = 0; j < matrix[0].length; j++) {
-				if(matrix[i][j] < maxValue) {
-					maxValue = matrix[i][j];
+			for(int j = 0; j < matrix.length; j++) {
+				if(matrix[j][i] > maxValue) {
+					maxValue = matrix[j][i];
 					maxIndex = j;
 				}
 			}
+
+			columnValueIndexArrayList.add(new int[]{maxValue, maxIndex});
 		}
+
+		List<Integer> list = new ArrayList<>();
+		for(int i = 0; i < rowValueIndexArrayList.size(); i++) {
+			int[] rowValueIndex = rowValueIndexArrayList.get(i);
+			int rowValue = rowValueIndex[0];
+			int rowIndex = rowValueIndex[1];
+
+			int[] columnValueIndex = columnValueIndexArrayList.get(rowIndex);
+			int columnValue = columnValueIndex[0];
+			int columnIndex = columnValueIndex[1];
+
+			if(rowValue == columnValue && columnIndex == i) {
+				list.add(rowValue);
+			}
+		}
+
+		return list;
 	}
 }
