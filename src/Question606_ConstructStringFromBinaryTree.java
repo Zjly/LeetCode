8018c89deb85ba14222f6d16a9a4bf4db4aaea6e
Zhang Lei <942471854@qq.com>
2022-03-19 18:22:52 +0800

    Question606_Construct String From Binary Tree

---
+++ src/Question606_ConstructStringFromBinaryTree.java
@@ -0,0 +1,86 @@
+/**
+ * 606. 根据二叉树创建字符串
+ * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+ * <p>
+ * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 二叉树: [1,2,3,4]
+ * 1
+ * /   \
+ * 2     3
+ * /
+ * 4
+ * <p>
+ * 输出: "1(2(4))(3)"
+ * <p>
+ * 解释: 原本将是“1(2(4)())(3())”，
+ * 在你省略所有不必要的空括号对之后，
+ * 它将是“1(2(4))(3)”。
+ * 示例 2:
+ * <p>
+ * 输入: 二叉树: [1,2,3,null,4]
+ * 1
+ * /   \
+ * 2     3
+ * \
+ * 4
+ * <p>
+ * 输出: "1(2()(4))(3)"
+ * <p>
+ * 解释: 和第一个示例相似，
+ * 除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+ */
+
+public class Question606_ConstructStringFromBinaryTree {
+	public static void main(String[] args) {
+
+	}
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution606 {
+	public String tree2str(TreeNode root) {
+		StringBuilder stringBuilder = new StringBuilder();
+		traverse(root, stringBuilder);
+		return stringBuilder.toString();
+	}
+
+	public void traverse(TreeNode root, StringBuilder tree) {
+		if(root == null) {
+		    return;
+		}
+		
+		tree.append(root.val);
+		if(root.left != null) {
+		    tree.append("(");
+			traverse(root.left, tree);
+			tree.append(")");
+		}
+		
+		if(root.right != null) {
+		    if(root.left == null) {
+		        tree.append("()");
+		    }
+
+			tree.append("(");
+			traverse(root.right, tree);
+			tree.append(")");
+		}
+	}
+}
