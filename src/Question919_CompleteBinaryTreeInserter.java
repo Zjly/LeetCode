7116a59cf82d395a26ca82ba12a2e2b22b17e22a
Zhang Lei <942471854@qq.com>
2022-07-25 16:15:08 +0800

    Question919_Complete Binary Tree Inserter

--- src/Question919_CompleteBinaryTreeInserter.java
+++ src/Question919_CompleteBinaryTreeInserter.java
@@ -4,35 +4,35 @@ import java.util.Queue;
 /**
  * 919. 完全二叉树插入器
  * 完全二叉树 是每一层（除最后一层外）都是完全填充（即，节点数达到最大）的，并且所有的节点都尽可能地集中在左侧。
- *
+ * <p>
  * 设计一种算法，将一个新节点插入到一个完整的二叉树中，并在插入后保持其完整。
- *
+ * <p>
  * 实现 CBTInserter 类:
- *
+ * <p>
  * CBTInserter(TreeNode root) 使用头节点为 root 的给定树初始化该数据结构；
  * CBTInserter.insert(int v)  向树中插入一个值为 Node.val == val的新节点 TreeNode。使树保持完全二叉树的状态，并返回插入节点 TreeNode 的父节点的值；
  * CBTInserter.get_root() 将返回树的头节点。
- *
- *
+ * <p>
+ * <p>
  * 示例 1：
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 输入
  * ["CBTInserter", "insert", "insert", "get_root"]
  * [[[1, 2]], [3], [4], []]
  * 输出
  * [null, 1, 2, [1, 2, 3, 4]]
- *
+ * <p>
  * 解释
  * CBTInserter cBTInserter = new CBTInserter([1, 2]);
  * cBTInserter.insert(3);  // 返回 1
  * cBTInserter.insert(4);  // 返回 2
  * cBTInserter.get_root(); // 返回 [1, 2, 3, 4]
- *
- *
+ * <p>
+ * <p>
  * 提示：
- *
+ * <p>
  * 树中节点数量范围为 [1, 1000]
  * 0 <= Node.val <= 5000
  * root 是完全二叉树
@@ -42,7 +42,13 @@ import java.util.Queue;
 
 public class Question919_CompleteBinaryTreeInserter {
 	public static void main(String[] args) {
+		TreeNode t1 = new TreeNode(1);
+		t1.left = new TreeNode(2);
 
+		CBTInserter cbtInserter = new CBTInserter(t1);
+		cbtInserter.insert(3);
+		cbtInserter.insert(4);
+		TreeNode p = cbtInserter.get_root();
 	}
 }
 
@@ -63,31 +69,53 @@ public class Question919_CompleteBinaryTreeInserter {
  */
 class CBTInserter {
 	Queue<TreeNode> queue;
-	TreeNode p;
+	TreeNode root;
 
 	public CBTInserter(TreeNode root) {
 		queue = new LinkedList<>();
 		queue.offer(root);
 
-		int full = 1;
+		this.root = root;
 
 		while(true) {
-		    int size = queue.size();
-			if(size == full) {
-				for(int i = 0; i < size; i++) {
-
-				}
+			TreeNode p = queue.peek();
+			if(p.left != null) {
+				queue.offer(p.left);
+			} else {
+				break;
 			}
 
+			if(p.right != null) {
+				queue.offer(p.right);
+				queue.poll();
+			} else {
+				break;
+			}
 		}
 	}
 
 	public int insert(int val) {
+		if(queue.peek() != null) {
+			TreeNode p = queue.peek();
+			if(p.left == null) {
+				TreeNode newNode = new TreeNode(val);
+				p.left = newNode;
+				queue.offer(newNode);
+			} else if(p.right == null) {
+				TreeNode newNode = new TreeNode(val);
+				p.right = newNode;
+				queue.poll();
+				queue.offer(newNode);
+			}
 
+			return p.val;
+		} else {
+			return -1;
+		}
 	}
 
 	public TreeNode get_root() {
-
+		return root;
 	}
 }
 
