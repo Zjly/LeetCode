be57c3caffefd2abd9286924f8b220d22efed9ad
Zhang Lei <942471854@qq.com>
2022-10-26 23:14:11 +0800

    Question129_SumRootToLeafNumbers

---
+++ src/Question129_SumRootToLeafNumbers.java
@@ -0,0 +1,83 @@
+/**
+ * 129. 求根节点到叶节点数字之和
+ * 给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
+ * 每条从根节点到叶节点的路径都代表一个数字：
+ * <p>
+ * 例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
+ * 计算从根节点到叶节点生成的 所有数字之和 。
+ * <p>
+ * 叶节点 是指没有子节点的节点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3]
+ * 输出：25
+ * 解释：
+ * 从根到叶子节点路径 1->2 代表数字 12
+ * 从根到叶子节点路径 1->3 代表数字 13
+ * 因此，数字总和 = 12 + 13 = 25
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [4,9,0,5,1]
+ * 输出：1026
+ * 解释：
+ * 从根到叶子节点路径 4->9->5 代表数字 495
+ * 从根到叶子节点路径 4->9->1 代表数字 491
+ * 从根到叶子节点路径 4->0 代表数字 40
+ * 因此，数字总和 = 495 + 491 + 40 = 1026
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点的数目在范围 [1, 1000] 内
+ * 0 <= Node.val <= 9
+ * 树的深度不超过 10
+ */
+
+public class Question129_SumRootToLeafNumbers {
+	public static void main(String[] args) {
+
+	}
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution129 {
+	int sum = 0;
+	public int sumNumbers(TreeNode root) {
+		dfs(root, 0);
+		return sum;
+	}
+
+	public void dfs(TreeNode root, int num) {
+		if(root == null) {
+		    return;
+		}
+
+		num = num * 10 + root.val;
+
+		if(root.left == null && root.right == null) {
+			sum += num;
+		}
+
+		dfs(root.left, num);
+		dfs(root.right, num);
+	}
+}
