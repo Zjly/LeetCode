171ddc11f47ed84aff89ed0e34257ef2aca01467
Zhang Lei <942471854@qq.com>
2022-10-29 00:41:31 +0800

    Question1295_FindNumbersWithEvenNumberOfDigits

---
+++ src/Question1295_FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,48 @@
+/**
+ * 1295. 统计位数为偶数的数字
+ * 给你一个整数数组 nums，请你返回其中位数为 偶数 的数字的个数。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [12,345,2,6,7896]
+ * 输出：2
+ * 解释：
+ * 12 是 2 位数字（位数为偶数）
+ * 345 是 3 位数字（位数为奇数）
+ * 2 是 1 位数字（位数为奇数）
+ * 6 是 1 位数字 位数为奇数）
+ * 7896 是 4 位数字（位数为偶数）
+ * 因此只有 12 和 7896 是位数为偶数的数字
+ * 示例 2：
+ * <p>
+ * 输入：nums = [555,901,482,1771]
+ * 输出：1
+ * 解释：
+ * 只有 1771 是位数为偶数的数字。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 500
+ * 1 <= nums[i] <= 10^5
+ */
+
+public class Question1295_FindNumbersWithEvenNumberOfDigits {
+	public static void main(String[] args) {
+
+	}
+}
+
+class Solution1295 {
+	public int findNumbers(int[] nums) {
+		int count = 0;
+		for(int num : nums) {
+			if(String.valueOf(num).length() % 2 == 0) {
+			    count++;
+			}
+		}
+		return count;
+	}
+}
