15adf873722fa81444e0763f3c0c0a978d4ee415
Zhang Lei <942471854@qq.com>
2023-06-18 12:23:49 +0800

    Question200_岛屿数量

---
+++ src/Question200_岛屿数量.java
@@ -0,0 +1,71 @@
+/**
+ * 200. 岛屿数量
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * <p>
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * <p>
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：grid = [
+ * ["1","1","1","1","0"],
+ * ["1","1","0","1","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * 输入：grid = [
+ * ["1","1","0","0","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","1","0","0"],
+ * ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] 的值为 '0' 或 '1'
+ */
+
+/**
+ * @author Zhang Lei
+ * @date 2023/6/18 12:21
+ */
+public class Question200_岛屿数量 {
+}
+
+class Solution200 {
+	public int numIslands(char[][] grid) {
+		int[][] dirs = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+		int count = 0;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == '1') {
+					count++;
+					dfs(grid, dirs, i, j);
+				}
+			}
+		}
+
+		return count;
+	}
+
+	private void dfs(char[][] grid, int[][] dirs, int x, int y) {
+		if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == '1') {
+			grid[x][y] = '0';
+			for (int[] dir : dirs) {
+				dfs(grid, dirs, x + dir[0], y + dir[1]);
+			}
+		}
+	}
+}
