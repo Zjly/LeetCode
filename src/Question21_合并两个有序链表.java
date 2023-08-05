15987b78286eb578d939fe1938dde0d678bb74f8
Zhang Lei <942471854@qq.com>
2023-08-05 23:29:28 +0800

    Question21_合并两个有序链表

---
+++ src/Question21_合并两个有序链表.java
@@ -0,0 +1,81 @@
+/**
+ * 21. 合并两个有序链表
+ * 简单
+ * 3.2K
+ * 相关企业
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ * <p>
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ */
+
+public class Question21_合并两个有序链表 {
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        } else if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+
+        ListNode head = new ListNode();
+        ListNode p = head;
+        head.next = list1.val <= list2.val ? list1 : list2;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                p.next = list1;
+                list1 = list1.next;
+            } else {
+                p.next = list2;
+                list2 = list2.next;
+            }
+
+            p = p.next;
+        }
+
+        if (list1 != null) {
+            p.next = list1;
+        }
+
+        if (list2 != null) {
+            p.next = list2;
+        }
+
+        return head.next;
+    }
+}
