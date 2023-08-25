5f8bac4042eb98667a5ba58b13bbd831db51113b
Zhang Lei <942471854@qq.com>
2023-08-25 23:20:11 +0800

    Question1448_统计二叉树中好节点的数目

---
+++ src/main/java/Question1448_统计二叉树中好节点的数目.java
@@ -0,0 +1,80 @@
+/**
+ * 1448. 统计二叉树中好节点的数目
+ * 给你一棵根为 root 的二叉树，请你返回二叉树中好节点的数目。
+ * <p>
+ * 「好节点」X 定义为：从根到该节点 X 所经过的节点中，没有任何节点的值大于 X 的值。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root = [3,1,4,3,null,1,5]
+ * 输出：4
+ * 解释：图中蓝色节点为好节点。
+ * 根节点 (3) 永远是个好节点。
+ * 节点 4 -> (3,4) 是路径中的最大值。
+ * 节点 5 -> (3,4,5) 是路径中的最大值。
+ * 节点 3 -> (3,1,3) 是路径中的最大值。
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root = [3,3,null,4,2]
+ * 输出：3
+ * 解释：节点 2 -> (3, 3, 2) 不是好节点，因为 "3" 比它大。
+ * 示例 3：
+ * <p>
+ * 输入：root = [1]
+ * 输出：1
+ * 解释：根节点是好节点。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 二叉树中节点数目范围是 [1, 10^5] 。
+ * 每个节点权值的范围是 [-10^4, 10^4] 。
+ */
+
+public class Question1448_统计二叉树中好节点的数目 {
+}
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * @author Zhang Lei
+ * @date 2023/8/25 23:16
+ */
+class Solution1448 {
+    public int goodNodes(TreeNode root) {
+        return dfs(root, Integer.MIN_VALUE);
+    }
+
+    private int dfs(TreeNode root, int maxValue) {
+        if (root == null) {
+            return 0;
+        }
+
+        int count = root.val >= maxValue ? 1 : 0;
+        maxValue = Math.max(maxValue, root.val);
+        count += dfs(root.left, maxValue);
+        count += dfs(root.right, maxValue);
+
+        return count;
+    }
+}
