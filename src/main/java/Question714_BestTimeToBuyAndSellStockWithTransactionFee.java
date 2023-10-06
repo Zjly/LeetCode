8da3f09bf1c879afab4092d5503b7ff093f5ff12
Zhang Lei <942471854@qq.com>
2023-10-06 17:23:07 +0800

    Question714_BestTimeToBuyAndSellStockWithTransactionFee

--- src/main/java/Question714_BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/main/java/Question714_BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -46,3 +46,20 @@ class Solution714 {
 		return profit;
 	}
 }
+
+class Solution714_2 {
+	public int maxProfit(int[] prices, int fee) {
+		int length = prices.length;
+		int[][] dp = new int[length][2];
+
+		dp[0][0] = -prices[0];
+		dp[0][1] = 0;
+
+		for (int i = 1; i < length; i++) {
+			dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+			dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+		}
+
+		return dp[length - 1][1];
+	}
+}
