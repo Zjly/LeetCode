fbf12fc4cbffbd21a9680be5a2d92343feef6caf
Zhang Lei <942471854@qq.com>
2023-10-15 16:14:54 +0800

    Question136_只出现一次的数字

---
+++ src/main/java/Question136_只出现一次的数字.java
@@ -0,0 +1,46 @@
+/**
+ * 136. 只出现一次的数字
+ * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * <p>
+ * 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1 ：
+ * <p>
+ * 输入：nums = [2,2,1]
+ * 输出：1
+ * 示例 2 ：
+ * <p>
+ * 输入：nums = [4,1,2,1,2]
+ * 输出：4
+ * 示例 3 ：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：1
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 3 * 104
+ * -3 * 104 <= nums[i] <= 3 * 104
+ * 除了某个元素只出现一次以外，其余每个元素均出现两次。
+ */
+
+public class Question136_只出现一次的数字 {
+}
+
+/**
+ * @author Zhang Lei
+ * @date 2023/10/14 14:05
+ */
+class Solution136 {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
