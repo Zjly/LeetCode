7f7c8c21a34934fc648df7f8f8438da911d6c59d
Zhang Lei <942471854@qq.com>
2023-10-27 20:02:02 +0800

    Question1465_切割后面积最大的蛋糕

---
+++ src/main/java/Question1465_切割后面积最大的蛋糕.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+
+/**
+ * 1465. 切割后面积最大的蛋糕
+ * 提示
+ * 1445
+ * 87
+ * 第 191 场周赛
+ * Q2
+ * 相关企业
+ * 矩形蛋糕的高度为 h 且宽度为 w，给你两个整数数组 horizontalCuts 和 verticalCuts，其中：
+ * <p>
+ * horizontalCuts[i] 是从矩形蛋糕顶部到第  i 个水平切口的距离
+ * verticalCuts[j] 是从矩形蛋糕的左侧到第 j 个竖直切口的距离
+ * 请你按数组 horizontalCuts 和 verticalCuts 中提供的水平和竖直位置切割后，请你找出 面积最大 的那份蛋糕，并返回其 面积 。由于答案可能是一个很大的数字，因此需要将结果 对 109 + 7 取余 后返回。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：h = 5, w = 4, horizontalCuts = [1,2,4], verticalCuts = [1,3]
+ * 输出：4
+ * 解释：上图所示的矩阵蛋糕中，红色线表示水平和竖直方向上的切口。切割蛋糕后，绿色的那份蛋糕面积最大。
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：h = 5, w = 4, horizontalCuts = [3,1], verticalCuts = [1]
+ * 输出：6
+ * 解释：上图所示的矩阵蛋糕中，红色线表示水平和竖直方向上的切口。切割蛋糕后，绿色和黄色的两份蛋糕面积最大。
+ * 示例 3：
+ * <p>
+ * 输入：h = 5, w = 4, horizontalCuts = [3], verticalCuts = [3]
+ * 输出：9
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= h, w <= 109
+ * 1 <= horizontalCuts.length <= min(h - 1, 105)
+ * 1 <= verticalCuts.length <= min(w - 1, 105)
+ * 1 <= horizontalCuts[i] < h
+ * 1 <= verticalCuts[i] < w
+ * 题目数据保证 horizontalCuts 中的所有元素各不相同
+ * 题目数据保证 verticalCuts 中的所有元素各不相同
+ */
+
+public class Question1465_切割后面积最大的蛋糕 {
+}
+
+/**
+ * @author Zhang Lei
+ * @date 2023/10/27 19:57
+ */
+class Solution1465 {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+
+        int maxHor = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length - 1]);
+        for (int i = 1; i < horizontalCuts.length; i++) {
+            maxHor = Math.max(horizontalCuts[i] - horizontalCuts[i - 1], maxHor);
+        }
+
+        int maxVer = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length - 1]);
+        for (int i = 1; i < verticalCuts.length; i++) {
+            maxVer = Math.max(verticalCuts[i] - verticalCuts[i - 1], maxVer);
+        }
+
+        long ans = ((long)maxHor * maxVer) % 1000000007;
+        return (int)ans;
+    }
+}
