6a95d19598bdabcd6bf7408b3dd5ad7fe4f073c1
Zhang Lei <942471854@qq.com>
2023-10-29 19:39:07 +0800

    Question274_HIndex

--- src/main/java/Question274_HIndex.java
+++ src/main/java/Question274_HIndex.java
@@ -31,3 +31,17 @@ class Solution274 {
 		return h;
 	}
 }
+
+class Solution274_2 {
+	public int hIndex(int[] citations) {
+		Arrays.sort(citations);
+		int h = 0;
+		for (int i = citations.length - 1; i >= 0; i--) {
+			if (citations[i] > h) {
+			    h++;
+			}
+		}
+
+		return h;
+	}
+}
