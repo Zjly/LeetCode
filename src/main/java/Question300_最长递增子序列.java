4bc126c7093708d21a92c8476c27685f6358f3be
Zhang Lei <942471854@qq.com>
2023-12-22 15:07:56 +0800

    Question300_最长递增子序列

---
+++ src/main/java/Question300_最长递增子序列.java
@@ -0,0 +1,63 @@
+/**
+ * 300. 最长递增子序列
+ * 中等
+ * 3.5K
+ * 相关企业
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * <p>
+ * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,1,0,3,2,3]
+ * 输出：4
+ * 示例 3：
+ * <p>
+ * 输入：nums = [7,7,7,7,7,7,7]
+ * 输出：1
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 2500
+ * -104 <= nums[i] <= 104
+ * <p>
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能将算法的时间复杂度降低到 O(n log(n)) 吗?
+ */
+
+public class Question300_最长递增子序列 {
+}
+
+/**
+ * @author Zhang Lei
+ * @date 2023/12/22 15:03
+ */
+class Solution300 {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+
+        dp[0] = 1;
+        int result = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            result = Math.max(result, dp[i]);
+        }
+
+        return result;
+    }
+}
