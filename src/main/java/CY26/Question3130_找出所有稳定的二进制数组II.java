5eba3ec24cdeecd4a78bde42e2eb36e0a7e31ba5
Zhang Lei <942471854@qq.com>
2026-03-10 00:23:27 +0800

    Question3130_找出所有稳定的二进制数组II

---
+++ src/main/java/CY26/Question3130_找出所有稳定的二进制数组II.java
@@ -0,0 +1,56 @@
+package CY26;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ZhangLei
+ * @version 2026/03/10 00:21
+ */
+public class Question3130_找出所有稳定的二进制数组II {
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        final long MOD = 1000000007;
+
+        long[][][] dp = new long[zero + 1][one + 1][2];
+        for (int i = 1; i <= Math.min(zero, limit); i++) {
+            dp[i][0][0] = 1;
+        }
+        for (int j = 1; j <= Math.min(one, limit); j++) {
+            dp[0][j][1] = 1;
+        }
+
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
+                if (i >= limit + 1) {
+                    dp[i][j][0] = dp[i][j][0] - dp[i - limit - 1][j][1] + MOD;
+                }
+                dp[i][j][0] = dp[i][j][0] % MOD;
+
+                dp[i][j][1] = dp[i][j - 1][0] + dp[i][j - 1][1];
+                if (j >= limit + 1) {
+                    dp[i][j][1] = dp[i][j][1] - dp[i][j - limit - 1][0] + MOD;
+                }
+                dp[i][j][1] = dp[i][j][1] % MOD;
+            }
+        }
+
+        long res = (dp[zero][one][0] + dp[zero][one][1]) % MOD;
+        return Math.toIntExact(res);
+    }
+
+    @Test
+    public void test1() {
+        Assertions.assertEquals(2, numberOfStableArrays(1, 1, 2));
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertEquals(1, numberOfStableArrays(1, 2, 1));
+    }
+
+    @Test
+    public void test3() {
+        Assertions.assertEquals(14, numberOfStableArrays(3, 3, 2));
+    }
+}
